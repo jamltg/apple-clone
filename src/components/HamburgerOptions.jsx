beb3dde62826b331eb3e9861988dfc6cee817b26
jamltg <jamletigio@gmail.com>
2024-05-09 19:16:55 +0800

    setup hamburger header

---
+++ src/components/HamburgerOptions.jsx
@@ -0,0 +1,18 @@
+export default function HamburgerOptions() {
+  return (
+    <div className="h-screen bg-[rgb(24,24,24)] px-8">
+      <ul className="text-[rgb(209,209,209)] font-semibold text-3xl">
+        <li className="py-2">Store</li>
+        <li className="py-2">Mac</li>
+        <li className="py-2">iPad</li>
+        <li className="py-2">iPhone</li>
+        <li className="py-2">Watch</li>
+        <li className="py-2">AirPods</li>
+        <li className="py-2">TV & Home</li>
+        <li className="py-2">Entertainment</li>
+        <li className="py-2">Accessories</li>
+        <li className="py-2">Support</li>
+      </ul>
+    </div>
+  )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,60 @@
 import { IoLogoApple, IoBagOutline } from "react-icons/io5";
 import { IoIosSearch } from "react-icons/io";
+import { Cross as Hamburger } from 'hamburger-react'
+import { useEffect, useState } from "react";
+import HamburgerOptions from "./HamburgerOptions";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+
+    // Cleanup function
+    return () => {
+      document.body.classList.remove('no-scroll');
+    };
+  }, [isOpen]);
+
   return (
-    <div className="h-20 bg-[rgb(24,24,24)]">
-      <ul className="w-full h-full flex justify-center items-center text-[rgb(209,209,209)]">
-        <div className="pr-6">
-          <IoLogoApple color={'rgb(209,209,209)'} size={20}/>
-        </div>
-        <div className="hidden lg:flex lg:items-center lg:gap-6 lg:text-sm lg:font-semibold">
-          <li>Store</li>
-          <li>Mac</li>
-          <li>iPad</li>
-          <li>iPhone</li>
-          <li>Watch</li>
-          <li>AirPods</li>
-          <li>TV & Home</li>
-          <li>Entertainment</li>
-          <li>Accessories</li>
-          <li>Support</li>
-        </div>
-        <div className="flex ml-6 gap-6"> 
-          <IoIosSearch color={'rgb(209,209,209)'} size={20}/>
-          <IoBagOutline color={'rgb(209,209,209)'} size={20} />
-        </div>
-      </ul>
+    <div className="bg-[rgb(24,24,24)]">
+      <div className="h-20 w-full px-4">
+        <ul className="h-full flex justify-between items-center lg:justify-center text-[rgb(209,209,209)]">
+          <div className="pr-6">
+            {!isOpen && <IoLogoApple color={'rgb(209,209,209)'} size={20}/>}
+          </div>
+          <div className="hidden lg:flex lg:items-center lg:gap-6 lg:text-sm lg:font-semibold">
+            <li>Store</li>
+            <li>Mac</li>
+            <li>iPad</li>
+            <li>iPhone</li>
+            <li>Watch</li>
+            <li>AirPods</li>
+            <li>TV & Home</li>
+            <li>Entertainment</li>
+            <li>Accessories</li>
+            <li>Support</li>
+          </div>
+          <div className="flex items-center ml-6"> 
+            {!isOpen && 
+              <div className="flex gap-5">
+                <IoIosSearch color={'rgb(209,209,209)'} size={22}/>
+                <IoBagOutline color={'rgb(209,209,209)'} size={20}/>
+              </div>
+            }
+            <div className="block ml-2 lg:hidden">
+              <Hamburger size={20} color="#D1D1D1" toggled={isOpen} toggle={setIsOpen}/>
+            </div>
+          </div>
+        </ul>
+      </div>
+      <div>
+        {isOpen && <HamburgerOptions/>}
+      </div> 
     </div>
   )
 }
