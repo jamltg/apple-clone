b88d0da5b49dd4fd3a3c7002411225b728ae6c69
jamltg <jamletigio@gmail.com>
2024-05-10 01:52:20 +0800

    fix padding of grid in homepage

--- src/components/HomeGrid.jsx
+++ src/components/HomeGrid.jsx
@@ -5,7 +5,7 @@ import GridMacbook from "./GridMacbook";
 
 export default function HomeGrid() {
   return (
-    <div className="p-4 grid grid-cols-1 gap-4 md:grid-cols-2 bg-[rgb(251,251,253)]">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 bg-[rgb(251,251,253)] md:p-4">
       <GridIphone/>
       <GridIwatch/>
       <GridAirpods/>
