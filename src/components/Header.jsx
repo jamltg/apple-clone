37423e94483a8ded9addcdc54fddf2f1310f75f0
jamltg <jamletigio@gmail.com>
2024-05-09 17:39:44 +0800

    setup basic header and react-router-dom

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div className="h-20 bg-[rgb(24,24,24)]">
+      <ul className="flex">
+        <li>Store</li>
+        <li>Mac</li>
+        <li>iPad</li>
+        <li>iPhone</li>
+        <li>Watch</li>
+        <li>Vision</li>
+      </ul>
+    </div>
+  )
+}
