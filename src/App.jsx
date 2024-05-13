d9f1805453fe1035282d43737deda6faafeca90c
jamltg <jamletigio@gmail.com>
2024-05-13 14:40:39 +0800

    delete files not part of apple homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import MacbookAir from "./pages/MacbookAir"
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/macbook-air" element={<MacbookAir/>}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
--- src/components/macbook/MacbookLead.jsx
+++
@@ -1,7 +0,0 @@
-export default function MacbookLead() {
-  return (
-    <div>
-      <h1 className="text-6xl font-bold">Lean. Mean. M3 machine.</h1>
-    </div>
-  )
-}
